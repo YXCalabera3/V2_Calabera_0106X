0cb1fb61ec03e5521b580f5be36a7a0f238e61d9
rodriku <script101a1@gmail.com>
2022-10-02 09:06:23 -0500

    v5

--- plugins/pago.js
+++ plugins/pago.js
@@ -9,6 +9,6 @@ type: 'audioMessage',
 ptt: true 
 })
 }
-handler.customPrefix = /yape|metodos de pago|metodo|pago|pagar|metodos|yape|Yape|YAPE|metodo|metodos|en que te pago|Yape|En que te pago|tienes yape|Tienes yape|tienes plim|Tienes plim|Tienes Plim|tienes bcp|en que te deposito|Tienes bcp|como te pago/
+handler.customPrefix = /yape|metodos de pago|metodo|pago|pagar|Pago|metodos|yape|Yape|YAPE|metodo|metodos|en que te pago|Yape|En que te pago|tienes yape|Tienes yape|tienes plim|Tienes plim|Tienes Plim|tienes bcp|en que te deposito|Tienes bcp|como te pago/
 handler.command = new RegExp
 export default handler
